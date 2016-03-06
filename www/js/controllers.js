5678999ff07d81ce56330bdf5c9d8c26cc3920c7
Nikita <nikitkakorzinka@gmail.com>
2016-03-06 23:09:00 +0200

    06.03
    Added tabs

---
+++ www/js/controllers.js
@@ -0,0 +1,15 @@
+angular.module('starter.controllers', [])
+
+.controller('InputCtrl', function($scope) {})
+
+.controller('CalculationCtrl', function($scope) {})
+
+
+
+.controller('ScheduleCtrl', function($scope) {})
+
+
+.controller('GraphCtrl', function($scope) {})
+
+
+.controller('SavedCtrl', function($scope) {});
